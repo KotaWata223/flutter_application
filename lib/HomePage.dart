4b2a4c8f84792a1c2a2047db6bdfc48290029dee
KotaWata223 <kotaxg-00W0@outlook.jp>
2025-05-12 13:46:14 +0900

    起動した最初の画面にカレンダーを表示させる

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,47 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/FirstPage.dart';
-import 'package:flutter_application_2/SecondPage.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:table_calendar/table_calendar.dart';  // Import intl package
 
-class HomePage extends StatelessWidget {
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  initializeDateFormatting('ja_JP').then((_) {  // Initialize locale
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center, // 中央揃え
-    children: [
-      OutlinedButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => FirstPage(),
-            ),
-          );
-        },
-        child: const Text('ログイン'),
-      ),
-      const SizedBox(height: 20), // ボタン間のスペース
-      OutlinedButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>  SecondPage(),
-            ),
-          );
-        },
-        child: const Text('新規登録'),
-      ),
-    ],
-  ),
-),
-
+      home: const HomePage(),
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('カレンダー'),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2000, 1, 1),
+            lastDay: DateTime.utc(2100, 12, 31),
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            locale: 'ja_JP',
+          ),
+        ],
+      ),
+    );
+  }
+}
